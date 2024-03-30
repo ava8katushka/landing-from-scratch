ab015de5409ed69d4f34967c4c5ac54c106423fb
Ava Katushkae <ava8katushka@gmail.com>
2024-03-30 13:09:17 +0400

    Made a menu

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,16 @@
-import './App.css';
-import 'bulma/css/bulma.min.css';
+import "./App.css";
+import "bulma/css/bulma.min.css";
+import Menu from "./components/Menu";
 
 function App() {
   return (
     <div className="App">
-      <button className="button is-black">Let's start it</button>
+      <div className="columns">
+        <div className="column is-one-third">
+          <Menu/>
+        </div>
+        <div className="column">Second column</div>
+      </div>
     </div>
   );
 }
